2c6a28aa04d3dd6f7e28a95bcaabe3b8dd174a10
Andriy Gaponov <wmconstructor@gmail.com>
2023-10-19 16:40:58 +0300

    add sql fill states

---
+++ src/main/resources/db/migration/V2023.10.19.002__add_country_states.sql
@@ -0,0 +1,31 @@
+insert into countries (name, alias) values ('Україна', 'Ukraine');
+
+insert into states (name, alias, country_id) values
+('Вінницька область', 'Vinnytska-oblast', 1),
+('Волинська область', 'Volynska-oblast', 1),
+('Дніпропетровська область', 'Dnipropetrovska-oblast', 1),
+('Донецька область', 'Donetska-oblast', 1),
+('Житомирська область', 'Zhytomyrska-oblast', 1),
+('Закарпатська область', 'Zakarpatska-oblast', 1),
+('Запорізька область', 'Zaporizka-oblast', 1),
+('Івано-Франківська область', 'Ivano-Frankivska-oblast', 1),
+('Київська область', 'Kyivska-oblast', 1),
+('Кіровоградська область', 'Kirovohradska-oblast', 1),
+('Луганська область', 'Luhanska-oblast', 1),
+('Львівська область', 'Lvivska-oblast', 1),
+('Миколаївська область', 'Mykolaivska-oblast', 1),
+('Одеська область', 'Odeska-oblast', 1),
+('Полтавська область', 'Poltavska-oblast', 1),
+('Рівненська область', 'Rivnenska-oblast', 1),
+('Сумська область', 'Sumska-oblast', 1),
+('Тернопільська область', 'Ternopilska-oblast', 1),
+('Харківська область', 'Kharkivska-oblast', 1),
+('Херсонська область', 'Khersonska-oblast', 1),
+('Хмельницька область', 'Khmelnytska-oblast', 1),
+('Черкаська область', 'Cherkaska-oblast', 1),
+('Чернівецька область', 'Chernivetska-oblast', 1),
+('Чернігівська область', 'Chernihivska-oblast', 1);
+('Чернігівська область', 'Avtonomna-Respublika-Krym', 1);
+
+
+
