6a9e27ddf0d73693dd1aba0bced5813736c08551
gaponov <wmconstructor@gmail.com>
2024-01-22 19:31:28 +0200

    add FilterDto

--- src/main/java/org/petmarket/advertisements/filter/dto/FilterDto.java
+++ src/main/java/org/petmarket/advertisements/filter/dto/FilterDto.java
@@ -1,10 +1,15 @@
 package org.petmarket.advertisements.filter.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.petmarket.advertisements.advertisement.dto.AdvertisementPriceRangeDto;
+import org.petmarket.advertisements.attributes.dto.AttributeGroupShortResponseDto;
+import org.petmarket.breeds.dto.BreedShortResponseDto;
 
 @Setter
 @Getter
@@ -13,6 +18,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class FilterDto {
 
-
-
+    @JsonProperty("lang_code")
+    private String langCode;
+    @JsonProperty("category_id")
+    private Long categoryId;
+    @JsonProperty("price_range")
+    private AdvertisementPriceRangeDto priceRange;
+    private List<BreedShortResponseDto> breeds;
+    @JsonProperty("favorite_attributes")
+    private List<AttributeGroupShortResponseDto> favoriteAttributes;
+    private AttributeGroupShortResponseDto attributes;
 }
