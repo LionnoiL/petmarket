1903e6d0272ae09b706774cb45eefd1817891250
gaponov <wmconstructor@gmail.com>
2024-01-22 19:17:54 +0200

    start add filter endpoint

---
+++ src/main/java/org/petmarket/advertisements/filter/controller/FilterController.java
@@ -0,0 +1,18 @@
+package org.petmarket.advertisements.filter.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Filter")
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@Validated
+@RequestMapping(value = "/v1/filter")
+public class FilterController {
+
+}
---
+++ src/main/java/org/petmarket/advertisements/filter/dto/FilterDto.java
@@ -0,0 +1,18 @@
+package org.petmarket.advertisements.filter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FilterDto {
+
+
+
+}
