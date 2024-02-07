56d31fd2d87320b39ae253df1444334daeacd51b
gfton <gftonot@gmail.com>
2024-02-07 18:41:01 +0200

    fix deployment error with repository @Param

--- src/main/java/org/petmarket/media/storage/repository/MediaRepository.java
+++ src/main/java/org/petmarket/media/storage/repository/MediaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,5 +22,5 @@ public interface MediaRepository extends JpaRepository<Media, Long> {
             FROM Media m
             WHERE m.name LIKE CONCAT(:id, ' %')
             """)
-    boolean existsByStartingIdInName(Long id);
+    boolean existsByStartingIdInName(@Param("id") Long id);
 }
