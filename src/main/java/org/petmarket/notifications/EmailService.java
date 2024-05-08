40e5e0528b05caf20c265bd4bf2e23df9ebe60a9
Andriy Gaponov <wmconstructor@gmail.com>
2024-05-08 13:07:03 +0300

    fix EmailService. add send email after update user.

--- src/main/java/org/petmarket/notifications/EmailService.java
+++ src/main/java/org/petmarket/notifications/EmailService.java
@@ -3,6 +3,8 @@ package org.petmarket.notifications;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.petmarket.language.entity.Language;
+import org.petmarket.options.service.OptionsService;
 import org.petmarket.translate.TranslationMessages;
 import org.petmarket.translate.TranslationService;
 import org.petmarket.users.entity.User;
@@ -25,6 +27,7 @@ public class EmailService implements NotificationService {
 
     private final JavaMailSender emailSender;
     private final TranslationService translationService;
+    private final OptionsService optionsService;
     @Autowired
     private SpringTemplateEngine templateEngine;
     @Value("${spring.mail.username}")
@@ -33,14 +36,19 @@ public class EmailService implements NotificationService {
     private String siteName;
 
     @Override
-    public void send(NotificationType type, Map<String, Object> fields, User user) {
-        log.info("send message to {} with text: {}", user.getEmail(), fields.get("link"));
+    public void send(NotificationType type, Map<String, Object> fields, Language language, String email) {
+        log.info("send message to {} with text: {}", email, fields.get("link"));
 
         fields.put("mailtoLink", "mailto:" + emailFrom);
         fields.put("email", emailFrom);
         fields.put("footer", "© " + Helper.getCurrentYear() + " " + siteName);
 
-        String userLangCode = getUserLanguageCode(user);
+        String userLangCode;
+        if (language == null) {
+            userLangCode = optionsService.getDefaultSiteLanguage().getLangCode();
+        } else {
+            userLangCode = language.getLangCode();
+        }
         String templateName = getTemplateName(type, userLangCode);
 
         switch (type) {
@@ -48,29 +56,34 @@ public class EmailService implements NotificationService {
                     translationService.getTranslate(TranslationMessages.PASSWORD_CHANGE_REQUEST, userLangCode),
                     templateName,
                     fields,
-                    user
+                    email
             );
             case CHANGE_PASSWORD, UPDATE_PASSWORD -> sendEmailMessage(
                     translationService.getTranslate(TranslationMessages.PASSWORD_CHANGED_SUCCESSFULLY, userLangCode),
                     templateName,
                     fields,
-                    user
+                    email
             );
             case MAIL_UPDATE_SUCCESSFULLY -> sendEmailMessage(
                     translationService.getTranslate(TranslationMessages.MAIL_CHANGED_SUCCESSFULLY, userLangCode),
                     templateName,
                     fields,
-                    user
+                    email
             );
-            default -> log.info("error sending mail to {}", user.getEmail());
+            default -> log.info("error sending mail to {}", email);
         }
     }
 
-    private String getUserLanguageCode(User user) {
+    @Override
+    public void send(NotificationType type, Map<String, Object> fields, User user) {
+        send(type, fields, getUserLanguageCode(user), user.getEmail());
+    }
+
+    private Language getUserLanguageCode(User user) {
         if (user.getLanguage() == null) {
-            return "ua";
+            return optionsService.getDefaultSiteLanguage();
         } else {
-            return user.getLanguage().getLangCode();
+            return user.getLanguage();
         }
     }
 
@@ -83,12 +96,13 @@ public class EmailService implements NotificationService {
             case RESET_PASSWORD -> result.append("reset-password.html");
             case CHANGE_PASSWORD -> result.append("reset-password-successfully.html");
             case UPDATE_PASSWORD -> result.append("update-password-successfully.html");
+            case MAIL_UPDATE_SUCCESSFULLY -> result.append("update-email-successfully.html");
             default -> log.info("no template assigned for to {}", type);
         }
         return result.toString();
     }
 
-    private void sendEmailMessage(String subject, String template, Map<String, Object> fields, User user) {
+    private void sendEmailMessage(String subject, String template, Map<String, Object> fields, String email) {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = null;
         try {
@@ -99,13 +113,13 @@ public class EmailService implements NotificationService {
             context.setVariables(fields);
             String emailContent = templateEngine.process(template, context);
 
-            mimeMessageHelper.setTo(user.getEmail());
+            mimeMessageHelper.setTo(email);
             mimeMessageHelper.setSubject(subject);
             mimeMessageHelper.setFrom(emailFrom);
             mimeMessageHelper.setText(emailContent, true);
             emailSender.send(message);
         } catch (Exception e) {
-            log.info("error sending mail to {} with subject: {}", user.getEmail(), subject);
+            log.info("error sending mail to {} with subject: {}", email, subject);
         }
     }
 }
