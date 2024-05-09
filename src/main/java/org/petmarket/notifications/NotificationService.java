b37d5a8419bf604c5ddea59cf00e370ef9cbf4d0
Andriy Gaponov <wmconstructor@gmail.com>
2024-05-09 15:38:09 +0300

    fix

--- src/main/java/org/petmarket/notifications/NotificationService.java
+++ src/main/java/org/petmarket/notifications/NotificationService.java
@@ -5,6 +5,5 @@ import org.petmarket.users.entity.User;
 import java.util.Map;
 
 public interface NotificationService {
-
     void send(NotificationType type, Map<String, Object> fields, User user);
 }
