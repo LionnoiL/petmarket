0a3d52339b4e127d532b618a45f249a3f7be1f94
Bohdan Koval <gftonot@gmail.com>
2024-05-15 21:07:32 +0300

    added search with POST method

---
+++ src/main/java/org/petmarket/advertisements/advertisement/dto/AdvertisementSearchRequestDto.java
@@ -0,0 +1,60 @@
+package org.petmarket.advertisements.advertisement.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.petmarket.advertisements.advertisement.entity.AdvertisementSortOption;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdvertisementSearchRequestDto {
+    @Schema(example = "dog")
+    @JsonProperty("search_term")
+    private String searchTerm;
+
+    @Schema(example = "1")
+    @JsonProperty(value = "page")
+    private int page;
+
+    @Schema(example = "10")
+    @JsonProperty(value = "size")
+    private int size;
+
+    @Schema(example = "1")
+    @JsonProperty("breed_ids")
+    private List<Long> breedIds;
+
+    @Schema(example = "1")
+    @JsonProperty("attribute_ids")
+    private List<Long> attributeIds;
+
+    @Schema(example = "1")
+    @JsonProperty("states_ids")
+    private List<Long> statesIds;
+
+    @Schema(example = "1")
+    @JsonProperty("city_ids")
+    private List<Long> cityIds;
+
+    @Schema(example = "1")
+    @JsonProperty("min_price")
+    private BigDecimal minPrice;
+
+    @Schema(example = "1000")
+    @JsonProperty("max_price")
+    private BigDecimal maxPrice;
+
+    @Schema(example = "1")
+    @JsonProperty(value = "sort_option")
+    private AdvertisementSortOption sortOption;
+
+    @Schema(example = "1")
+    @JsonProperty("category_id")
+    private Long categoryId;
+}
