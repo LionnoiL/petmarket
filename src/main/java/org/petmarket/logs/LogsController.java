1d68ab402b0d22f8ab19c1c3873241e554406b68
Bohdan Koval <gftonot@gmail.com>
2024-05-31 14:35:36 +0300

    making logs endpoint return file instead of text

--- src/main/java/org/petmarket/logs/LogsController.java
+++ src/main/java/org/petmarket/logs/LogsController.java
@@ -6,15 +6,19 @@ import org.petmarket.utils.annotations.responses.ApiResponseForbidden;
 import org.petmarket.utils.annotations.responses.ApiResponseSuccessful;
 import org.petmarket.utils.annotations.responses.ApiResponseUnauthorized;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,11 +32,22 @@ public class LogsController {
     @ApiResponseUnauthorized
     @ApiResponseForbidden
     @GetMapping
-    public ResponseEntity<List<String>> getLogs() {
+    public ResponseEntity<Resource> getLogs() {
         try {
-            return ResponseEntity.ok(Files.readAllLines(Paths.get(logFilePath)));
+            File file = new File(logFilePath);
+
+            if (!file.exists() || !file.isFile()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            Resource resource = new FileSystemResource(file);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+            headers.add(HttpHeaders.CONTENT_TYPE, Files.probeContentType(Paths.get(logFilePath)));
+
+            return new ResponseEntity<>(resource, headers, HttpStatus.OK);
         } catch (IOException e) {
-            return ResponseEntity.status(500).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 }
