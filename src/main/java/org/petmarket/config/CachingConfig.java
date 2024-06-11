04522760b9d5601cb058e85f6b0989c1d459dc2d
KovalBohdan-0 <gftonot@gmail.com>
2024-06-11 18:17:51 +0300

    updating cache config

--- src/main/java/org/petmarket/config/CachingConfig.java
+++ src/main/java/org/petmarket/config/CachingConfig.java
@@ -1,16 +1,30 @@
 package org.petmarket.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CachingConfig {
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("users");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("users");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(20, TimeUnit.MINUTES)
+                .weakKeys()
+                .recordStats();
     }
 }
